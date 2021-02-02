f3e22cf92fc80467b206c453195f650db0cc0994
Luiz Henrique Zamberlan <LuiZamberlan@outlook.com>
2021-02-02 02:44:21 +0000

    add main.lua and readme

---
+++ README.md
@@ -0,0 +1,13 @@
+# Repositório De Lua
+
+### Atenção
+* Todos os comentários são anotações que fizemos pesquisando definições na internet, somente no intuito de **anotar** e **aprender**
+* Não tenho nada de novo para trazer nesse repositório, somente uma forma de registrar o que aprendemos
+
+#### Fontes
+
+  * Manual [Lua.org](https://www.lua.org/manual/5.1/pt)
+  * Canal [Cria Jogo](https://www.youtube.com/channel/UC6BJ2X5Vr6h5TdT7tNY7iAA)
+	  * [Playlist de Lua](https://www.youtube.com/watch?v=_EyRsqA_tKA&list=PLqYboeh3Jru55Yq4J08zsBoOwwwjUtZNA)
+
+> De [Lucas Felipe](https://github.com/LucassFelipe/) e [Luiz Zamberlan](https://github.com/LuiZamberlan) :)
---
+++ main.lua
@@ -0,0 +1,59 @@
+-- comentário de uma linha
+
+--[[
+
+comentário
+
+longo....
+
+]]--
+
+print("Olá, Mundo!") --  O famoso programa "Olá, Mundo!"
+
+print("2 > 3?")
+print(2 > 3) -- é esperado retornar 'false'
+
+print("Quanto é 2 + 2?")
+print(2 + 2) -- vai somar dois mais dois. resultando em quatro.
+
+--[[ 
+	
+os outros operadores aritméticos:
+
+'-' para fazer subtrações
+'*' para fazer multiplicações
+'/' para fazer divisões
+'%' para calcular o módulo da divisão
+'^' para calcular exponenciação
+
+]]--
+
+ex1 = 'Opa!' -- isso aqui é uma variável!
+
+print(ex1) -- vc pode imprimir ela
+
+-- Operador de concatenação de Strings:
+
+ex1 = 'oi'
+-- Substituindo o valor de uma variável
+
+ex3 = ', tudo bem?'
+print(ex1 .. ex3)
+-- O mesmo ocorre com números e Strings
+
+numero = 1
+print(ex1 .. numero)
+
+-- Desafio aleatório do </Lucas>.
+function fatorial(n)
+	multiplicador = n - 1
+	f = n
+
+	while multiplicador > 1 do
+		f = f * multiplicador
+		multiplicador = multiplicador - 1
+	end
+  return print(f)
+end
+
+fatorial(5)
